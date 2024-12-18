ab986d329b56ccbaf922479b354a849df4a63262
CoderPEPE <esoph.dev.9@gmail.com>
2024-12-19 01:27:55 +0900

    chore: remove dev purpose admin navigation

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -22,11 +22,11 @@ const Admin: React.FC = () => {
   const [openAlertModal, setOpenAlertModal] = useState(false);
   const { isAdmin } = useAuth();
 
-  // useEffect(() => {
-  //   if (!isAdmin) {
-  //     setOpenAlertModal(true);
-  //   }
-  // }, [userInfo]);
+  useEffect(() => {
+    if (!isAdmin) {
+      setOpenAlertModal(true);
+    }
+  }, [userInfo]);
 
   const onClose = () => {
     setModalOpen(0);
