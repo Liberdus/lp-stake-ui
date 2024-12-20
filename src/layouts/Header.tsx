f21907ca95160003707acb6466ab82af2a5dea5d
CoderPEPE <esoph.dev.9@gmail.com>
2024-12-20 13:28:07 +0900

    chore: update logo and project name

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -93,7 +93,7 @@ const Header: React.FC = () => {
                 WebkitTextFillColor: 'transparent'
               }}
             >
-              Liber LP Staking
+              Liberdus LP Staking
             </Typography>
           </Box>
 
